3c8543ac510d4f4e0bc299b9f9a6fef4f6d2570b
Marc Morcos <marc.morcos1@hotmail.com>
2024-09-25 21:56:36 -0400

    Update code to work with my excel sheet

--- databaseWordCounter.py
+++ databaseWordCounter.py
@@ -5,7 +5,6 @@ import os
 import pandas as pd
 import numpy as np
 import time
-import traceback
 from tqdm import tqdm
 import json
 
@@ -21,44 +20,20 @@ def getWords(text):
     text = [re.sub("'s$", '', word) for word in text]
 
     return text
-
-#get brief_summary and detailed_description
-def getSectionsWeWant(filepath):  
-    with open(filepath, "r", encoding="utf8") as f:
-        data = json.load(f)
-        description = data["protocolSection"]["descriptionModule"]
-        text=description.get('briefSummary', "") + "\n" + data.get('detailedDescription', "")    
-        assert(text.strip()!="")
-    
-    return text
-    
-
-#what each thread will do
-#get a nice word list of the sections we want from an json file
-def preProcessItem(inputItem):
-    try:
-        filepath = os.path.join(os.getcwd(),"ctg-studies",inputItem["NCTNum"]+".json")
-        text = getSectionsWeWant(filepath)
-        words = getWords(text)
-        inputItem["words"] = words
-        return inputItem
-    except Exception as e:
-        print("error while processing")
-        print(inputItem)
-        traceback.print_exc()
-        raise e
         
 
 def main():
+    stats = dict() #used for other notes
+
     startTime = time.time()
     print("Starting")
 
-    wordsWeWant = "Depression,Anxiety,esteem,Schizophrenia,Borderline personality disorder,stress,personality,emasculation,humiliation,isolation,loneliness,frustration" #None
+    wordsWeWant = None #"Depression,Anxiety,esteem,Schizophrenia,Borderline personality disorder,stress,personality,emasculation,humiliation,isolation,loneliness,frustration" #None
     
     if(wordsWeWant is not None):
         wordsWeWant = getWords(wordsWeWant)
+        wordsWeWant = set([wordInList.lower() for wordInList in wordsWeWant]) #lowercase
         print("Edited wordswewant list to look like the following:",wordsWeWant)
-        wordsWeWant = [wordInList.lower() for wordInList in wordsWeWant] #lowercase
 
     #change directory to current file path
     abspath = os.path.abspath(__file__)
@@ -69,43 +44,50 @@ def main():
     outputDir = os.path.abspath("./Output")
     os.makedirs(outputDir, exist_ok = False)
 
-    #read original csv
-    statuses = dict()
-    toGet = []
-    inputFile = os.path.abspath("./ctg-studies.csv")
-    inputDF = pd.read_csv(inputFile, engine='python').dropna(how="all")
-    for index, row in inputDF.iterrows():
-        yearKeyToLookFor = "Start Date"
+    #load original excel file
+    studyDict = dict()
+    toProcess = []
+    inputFile = os.path.abspath("./PubMed_results.xlsx")
+    inputDF = pd.read_excel(inputFile).dropna(how="all")
+    stats["Num_papers_assuming_duplicated_already_filtered"] = len(inputDF)
+    
+    #parse original excel file
+    for index, row in tqdm(inputDF.iterrows(), total=len(inputDF), desc="Parsing Data", leave=True) :
+        #get year
+        yearKeyToLookFor = "pubDate"
         year = row[yearKeyToLookFor]
         if(type(year) != str):
-            year = "No "+yearKeyToLookFor
-        else:
-            year=year[:4]
+            continue #filter no year
+        
+        year = json.loads(year)
+        if("Year" not in year):
+            continue #filter no year
 
-        status = row["Study Status"].upper()
-        NCTNum = row["NCT Number"]
+        year=int(year["Year"])
+        if(year<1950 or year>2025):
+            raise Exception(f"Unexpected year val {year}")
+
+        # get abstract
+        abstract = str(row["Abstract"])
+        if(abstract.strip() == "" or abstract.strip() == "nan"):
+            continue #filter no abstract
+        abstract = getWords(abstract)
+        
 
         #prepare output
-        if status not in statuses:
-            statuses[status] = dict()
-        if year not in statuses[status]:
-            statuses[status][year] = [dict(),0] #[word dict, number of studies for that year]
+        if year not in studyDict:
+            studyDict[year] = [dict(),0] #[word dict, number of studies for that year]
         
         #prepare input
-        toGet.append({"year":year,
-                       "status":status,
-                       "NCTNum":NCTNum})
+        toProcess.append({"year":year,
+                       "words":abstract
+                       })
 
-
-    print("Starting file reads, total current runtime:",time.time()-startTime)
-    #read all the json files and get word list
-    results = [preProcessItem(item) for item in tqdm(toGet,desc= "Reading files and doing some preprocessing", leave=True)]
-    
-    print("Starting Finalization:",time.time()-startTime)
+    stats["num_papers_after_filter_no_year_or_no_abstract"] = len(toProcess)
 
     #count statistics
-    for study in tqdm(results,desc="Calculating statistics", leave=True) :
-        yearArr = statuses[study["status"]][study["year"]]
+    for study in tqdm(toProcess,desc="Calculating statistics", leave=True) :
+        yearArr = studyDict[study["year"]]
         yearDict = yearArr[0]
         yearArr[1] = yearArr[1]+1
         wordsFoundInCurrentStudy = set()
@@ -134,35 +116,37 @@ def main():
     #format data
     colTitles = ["word","number of mentions","Avg percent of mentions per study","percent of studies in year mentioning word"]
     colsPerYear = len(colTitles)
-    for statusKey,statusVal in tqdm(statuses.items(),desc="Formating data", leave=True):
-
-        #count max words
-        maxWords = 0
-        for yearVal in statusVal.values():
-            maxWords = max(maxWords,len(yearVal[0]))
-        
-        shape = (maxWords+2,colsPerYear*len(statusVal))
-        output =np.full(shape, "", dtype="object", order='C')
-
-        sortedYears = sorted(statusVal.items(), key=lambda item: int(item[0]) if item[0].isdecimal() else 99999999)
-        for yearInd,(yearKey,yearValArr) in enumerate(sortedYears):
-            yearVal = yearValArr[0]
-            #titles
-            col = yearInd*colsPerYear
-            output[0,col] = yearKey
-            output[0,col+1] = "Num studies:"+str(yearValArr[1])
-            output[1,col:col+colsPerYear]=colTitles
-
-            #values
-            sortedWords = sorted(yearVal.items(), key=lambda item: item[1][0],reverse =True)
-            for row,(wordKey,wordVal) in enumerate(sortedWords):
-                #for average, divide by number of studies in group (not len(wordVal[1]) since wordVal[1] has no entries of 0%)
-                output[row+2,col:col+colsPerYear]=[wordKey,wordVal[0],str(sum(wordVal[1])/yearValArr[1])+"%",str(100*wordVal[2]/yearValArr[1])+"%"]
+    #count max words
+    maxWords = 0
+    for yearVal in studyDict.values():
+        maxWords = max(maxWords,len(yearVal[0]))
     
-        #save the excel sheet with name of status
-        with open(os.path.join(outputDir,statusKey+'.csv'), 'w', newline='', encoding="utf-8-sig") as fp:
-                writer = csv.writer(fp, quoting=csv.QUOTE_NONNUMERIC)
-                writer.writerows(output.tolist())
+    shape = (maxWords+2,colsPerYear*len(studyDict))
+    output =np.full(shape, "", dtype="object", order='C')
+
+    sortedYears = sorted(studyDict.items(), key=lambda item: int(item[0]))
+    for yearInd,(yearKey,yearValArr) in tqdm(enumerate(sortedYears), total = len(studyDict), desc="Formating data", leave=True):
+        yearVal = yearValArr[0]
+        #titles
+        col = yearInd*colsPerYear
+        output[0,col] = yearKey
+        output[0,col+1] = "Num studies:"+str(yearValArr[1])
+        output[1,col:col+colsPerYear]=colTitles
+
+        #values
+        sortedWords = sorted(yearVal.items(), key=lambda item: item[1][0],reverse =True)
+        for row,(wordKey,wordVal) in enumerate(sortedWords):
+            #for average, divide by number of studies in group (not len(wordVal[1]) since wordVal[1] has no entries of 0%)
+            output[row+2,col:col+colsPerYear]=[wordKey,wordVal[0],str(sum(wordVal[1])/yearValArr[1])+"%",str(100*wordVal[2]/yearValArr[1])+"%"]
+
+    #save the excel sheet with name of status
+    with open(os.path.join(outputDir,'wordcount.csv'), 'w', newline='', encoding="utf-8-sig") as fp:
+            writer = csv.writer(fp, quoting=csv.QUOTE_NONNUMERIC)
+            writer.writerows(output.tolist())
+    
+    # write other stats
+    with open(os.path.join(outputDir,"otherData.txt"), 'w') as f:
+        f.writelines([json.dumps(stats, indent=4),])
     
     print("Done:",time.time()-startTime)
 
